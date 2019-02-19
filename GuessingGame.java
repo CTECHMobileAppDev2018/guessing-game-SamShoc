0a9d2376d31a3f72810b204d8ee9660d9ca282a8
SamShoc <7794501@isd535.org>
2019-02-19 09:04:33 -0600

    Getting user input and made a function game
    still need to find out how many times I guessed

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,10 +1,28 @@
-
+import java.util.Scanner;
+import java.util.Random;
 public class GuessingGame {
 
     public static void main(String[] args) {
-    
         System.out.println("Welcome to the guessing game.");
-    
+        Random r = new Random();
+        int high = 99;
+        int low = 1;
+        int n = r.nextInt(high - low) + low;
+        System.out.println("Pick a number between 1 and 100");
+        int userNum = -10;
+        while (n != userNum){
+            Scanner user = new Scanner(System.in);
+            userNum = user.nextInt();
+            if (userNum < n) {
+              System.out.println("That number is to low");
+            } else if (userNum > n) {
+              System.out.println("That number is to high");
+            } else {
+              System.out.println("You Win");
+            }
+            
+            
+            
+        }
     }
-
 }
